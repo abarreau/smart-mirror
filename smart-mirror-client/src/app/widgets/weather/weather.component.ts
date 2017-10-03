039880bdc71443a2899eb0d248f506f4d11ce566
Anthony Barreau <anthony.barreau@itk.fr>
2017-10-03 13:35:38 +0200

    Adds input for units in weather widget

--- smart-mirror-client/src/app/widgets/weather/weather.component.ts
+++ smart-mirror-client/src/app/widgets/weather/weather.component.ts
@@ -13,10 +13,13 @@ export class WeatherComponent implements OnInit {
   @Input()
   public city = "Paris";
 
+  @Input()
+  public units = "metric";
+
   @Input('api-key')
   public API_KEY: string; // TODO externaliser cette donnée
   private API_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=';
-  private API_OPTIONS = '&units=metric';
+  private API_OPTIONS = '&units=';
 
   public weather: Weather;
 
@@ -24,7 +27,7 @@ export class WeatherComponent implements OnInit {
 
   ngOnInit() {
     this.httpClient
-      .get(this.API_URL + this.API_KEY + "&q=" + this.city + this.API_OPTIONS)
+      .get(this.API_URL + this.API_KEY + "&q=" + this.city + this.API_OPTIONS + this.units)
       .subscribe((result) => {
         console.log(result);
         this.weather = jsonWeatherToModel(result);
