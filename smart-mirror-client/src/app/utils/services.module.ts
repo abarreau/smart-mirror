7cf3a55fce1c3937cb3894350d970413071be0d8
Anthony Barreau <anthony.barreau@itk.fr>
2018-04-24 11:17:15 +0200

    Adds logger server

---
+++ smart-mirror-client/src/app/utils/services.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { LoggerService } from './logger/logger.service';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  providers: [
+    LoggerService
+  ],
+})
+export class ServicesModule { }
